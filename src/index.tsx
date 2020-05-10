b5070a08f4ffc02ca7be6d6b5fea0a028eced4e6
wcywin <wcywin@gmail.com>
2020-05-10 22:27:22 +0200

    add styled-components and declaration in typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <React.StrictMode>
+      <div>Hello World</div>
+    </React.StrictMode>
+  </ThemeProvider>,
+document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
