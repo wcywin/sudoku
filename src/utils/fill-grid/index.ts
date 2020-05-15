c67630c2c12a5766bb5f48213247a2bbdbe421e8
wcywin <wcywin@gmail.com>
2020-05-15 08:57:34 +0200

    add the Shuffle function - the Fischer-Yates algorithm

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,4 +1,5 @@
-import { GRID } from 'typings'
+import { GRID, NUMBERS } from 'typings'
+import { Shuffle } from 'utils'
 
 const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,8 +13,29 @@ const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-function fillGrid(grid: GRID) {
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+/**
+ * A backtracking/recursive function to check all the possible combinations of numbers until a solution is found
+ * @param grid 9x9 Sudoku Grid
+ */
+function fillGrid(grid: GRID) {
+  let row = 0
+  let col = 0
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9)
+    col = i % 9
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers)
+      // Do stuff
+      // recursive things
+      break
+    }
+  }
+
+  grid[row][col] = 0
 }
 
 export default fillGrid
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as FillGrid } from './fill-grid'
+export { default as Shuffle } from './shuffle'
