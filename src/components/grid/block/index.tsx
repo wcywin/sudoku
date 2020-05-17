8301b18056d2c97145ae730044ef029b5690496d
wcywin <wcywin@gmail.com>
2020-05-17 22:19:15 +0200

    add test for the fillGrid function and update the Grid component

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
   return (
-    <Container data-cy={`block-${rowIndex}-${colIndex}`} />
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
   )
 }
 
--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,10 +1,25 @@
 import React, { FC, Children } from 'react'
 
+import { GRID } from 'typings'
+import { fillGrid } from 'utils'
+
 import Block from './block'
 import { Container, Row } from './styles'
 
 const Grid: FC = () => {
-  // Other stuff on the way
+  const grid: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+  fillGrid(grid)
+  console.log('grid', grid)
 
   return (
     <Container data-cy="grid-container">
