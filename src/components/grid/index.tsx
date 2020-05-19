3310d673392a384d3dfd3fc4c302c60c01af0d0e
wcywin <wcywin@gmail.com>
2020-05-19 08:42:50 +0200

    Dispatch create grid with useCallback and useEffect

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,13 +1,18 @@
-import React, { FC, Children } from 'react'
+import React, {FC, Children, useCallback, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
 
-import { createFullGrid } from 'utils'
+import { createGrid } from 'reducers'
 
 import Block from './block'
 import { Container, Row } from './styles'
 
 const Grid: FC = () => {
-  const grid = createFullGrid()
-  console.log('grid', grid)
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+  const create = useCallback(() => dispatch(createGrid()), [dispatch])
+  useEffect(() => {
+    create()
+  }, [create])
 
   return (
     <Container data-cy="grid-container">
