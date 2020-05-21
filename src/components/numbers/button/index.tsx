dfe7cf82a96563e770f8ece3723f6cba1874e68d
wcywin <wcywin@gmail.com>
2020-05-21 09:13:58 +0200

    add button mapping values and filling with dispatch action

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,15 +1,36 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
+
 import { Button } from 'components'
-import { NUMBERS } from 'typings'
+import { fillBlock, IReducer } from 'reducers'
+import { BLOCK_COORDS, N, NUMBERS } from 'typings'
 
 interface IProps {
   value: NUMBERS
 }
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS,
+  selectedValue: N
+}
+
 const NumberButton: FC<IProps> = ({ value }) => {
-  function fill() {
-    console.log('fill', value)
-  }
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue: workingGrid && selectedBlock
+        ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+        : 0
+  }))
+
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+  const fill = useCallback(() => {
+    if (state.selectedBlock && state.selectedValue === 0) {
+      dispatch(fillBlock(value, state.selectedBlock))
+    }
+  }, [dispatch, state.selectedBlock, state.selectedValue, value])
 
   return <Button onClick={fill}>{value}</Button>
 }
